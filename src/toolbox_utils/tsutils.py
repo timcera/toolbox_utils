5189d31dd6cdd91fbcf9273c4ca0e1d48472652b
Tim Cera <tim@cerazone.net>
2025-07-20 01:28:14 -0400

    fix: fix entries in pandas_offset_by_version

--- src/toolbox_utils/tsutils.py
+++ src/toolbox_utils/tsutils.py
@@ -52,6 +52,99 @@ def pandas_offset_by_version(new_offset: str) -> str:
     """
     Convert the time offset code to match the version of pandas.
 
+    +------------------------+--------+---------+-----------------------------+
+    | DateOffset             | less   | greater | Generic offset class,       |
+    |                        | 2.2    | equal   | defaults to absolute 24     |
+    |                        |        | 2.2     | hours                       |
+    +========================+========+=========+=============================+
+    | BDay or BusinessDay    | 'B'    | 'B'     | business day (weekday)      |
+    +------------------------+--------+---------+-----------------------------+
+    | CDay or                | 'C'    | 'C'     | custom business day         |
+    | CustomBusinessDay      |        |         |                             |
+    +------------------------+--------+---------+-----------------------------+
+    | Week                   | 'W'    | 'W'     | one week, optionally        |
+    |                        |        |         | anchored on a day of the    |
+    |                        |        |         | week                        |
+    +------------------------+--------+---------+-----------------------------+
+    | WeekOfMonth            | 'WOM'  | 'WOM'   | the x-th day of the y-th    |
+    |                        |        |         | week of each month          |
+    +------------------------+--------+---------+-----------------------------+
+    | LastWeekOfMonth        | 'LWOM' | 'LWOM'  | the x-th day of the last    |
+    |                        |        |         | week of each month          |
+    +------------------------+--------+---------+-----------------------------+
+    | MonthEnd               | 'M'    | 'ME'    | calendar month end          |
+    +------------------------+--------+---------+-----------------------------+
+    | MonthBegin             | 'MS'   | 'MS'    | calendar month begin        |
+    +------------------------+--------+---------+-----------------------------+
+    | BMonthEnd or           | 'BM'   | 'BME'   | business month end          |
+    | BusinessMonthEnd       |        |         |                             |
+    +------------------------+--------+---------+-----------------------------+
+    | BMonthBegin or         | 'BMS'  | 'BMS'   | business month begin        |
+    | BusinessMonthBegin     |        |         |                             |
+    +------------------------+--------+---------+-----------------------------+
+    | CBMonthEnd or          | 'CBM'  | 'CBME'  | custom business month end   |
+    | CustomBusinessMonthEnd |        |         |                             |
+    +------------------------+--------+---------+-----------------------------+
+    | CBMonthBegin or        | 'CBMS' | 'CBMS'  | custom business month begin |
+    | CustomBusinessMonthBeg |        |         |                             |
+    | in                     |        |         |                             |
+    +------------------------+--------+---------+-----------------------------+
+    | SemiMonthEnd           | 'SM'   | 'SME'   | 15th (or other              |
+    |                        |        |         | day_of_month) and calendar  |
+    |                        |        |         | month end                   |
+    +------------------------+--------+---------+-----------------------------+
+    | SemiMonthBegin         | 'SMS'  | 'SMS'   | 15th (or other              |
+    |                        |        |         | day_of_month) and calendar  |
+    |                        |        |         | month begin                 |
+    +------------------------+--------+---------+-----------------------------+
+    | QuarterEnd             | 'Q'    | 'QE'    | calendar quarter end        |
+    +------------------------+--------+---------+-----------------------------+
+    | QuarterBegin           | 'QS'   | 'QS'    | calendar quarter begin      |
+    +------------------------+--------+---------+-----------------------------+
+    | BQuarterEnd            | 'BQ    | 'BQE'   | business quarter end        |
+    +------------------------+--------+---------+-----------------------------+
+    | BQuarterBegin          | 'BQS'  | 'BQS'   | business quarter begin      |
+    +------------------------+--------+---------+-----------------------------+
+    | FY5253Quarter          | 'REQ'  | 'REQ'   | retail (aka 52-53 week)     |
+    |                        |        |         | quarter                     |
+    +------------------------+--------+---------+-----------------------------+
+    | YearEnd                | 'A'    | 'YE'    | calendar year end           |
+    +------------------------+--------+---------+-----------------------------+
+    | YearBegin              | 'AS'   | 'YS' or | calendar year begin         |
+    |                        | or     | 'BYS'   |                             |
+    |                        | 'BYS'  |         |                             |
+    +------------------------+--------+---------+-----------------------------+
+    | BYearEnd               | 'BA'   | 'BYE'   | business year end           |
+    +------------------------+--------+---------+-----------------------------+
+    | BYearBegin             | 'BAS'  | 'BYS'   | business year begin         |
+    +------------------------+--------+---------+-----------------------------+
+    | FY5253                 | 'RE'   | 'RE'    | retail (aka 52-53 week)     |
+    |                        |        |         | year                        |
+    +------------------------+--------+---------+-----------------------------+
+    | Easter                 | None   |         | Easter holiday              |
+    +------------------------+--------+---------+-----------------------------+
+    | BusinessHour           | 'BH'   | 'bh'    | business hour               |
+    +------------------------+--------+---------+-----------------------------+
+    | CustomBusinessHour     | 'CBH'  | 'cbh'   | custom business hour        |
+    +------------------------+--------+---------+-----------------------------+
+    | Day                    | 'D'    | 'D'     | one absolute day            |
+    +------------------------+--------+---------+-----------------------------+
+    | Hour                   | 'H'    | 'h'     | one hour                    |
+    +------------------------+--------+---------+-----------------------------+
+    | Minute                 | 'T' or | 'min'   | one minute                  |
+    |                        | 'min'  |         |                             |
+    +------------------------+--------+---------+-----------------------------+
+    | Second                 | 'S'    | 's'     | one second                  |
+    +------------------------+--------+---------+-----------------------------+
+    | Milli                  | 'L' or | 'ms'    | one millisecond             |
+    |                        | 'ms'   |         |                             |
+    +------------------------+--------+---------+-----------------------------+
+    | Micro                  | 'U' or | 'us'    | one microsecond             |
+    |                        | 'us'   |         |                             |
+    +------------------------+--------+---------+-----------------------------+
+    | Nano                   | 'N'    | 'ns'    | one nanosecond              |
+    +------------------------+--------+---------+-----------------------------+
+
     Parameters
     ----------
     offset
@@ -66,17 +159,43 @@ def pandas_offset_by_version(new_offset: str) -> str:
     major, minor = pd.__version__.split(".")[:2]
     if (int(major) + int(minor) / 10) < 2.2:
         new_to_old_freq = {
-            "Y": "A",
             "ME": "M",
             "BME": "BM",
-            "SME": "SM",
             "CBME": "CBM",
+            "SME": "SM",
             "QE": "Q",
+            "QE-JAN": "Q-JAN",
+            "QE-FEB": "Q-FEB",
+            "QE-MAR": "Q-MAR",
+            "QE-APR": "Q-APR",
+            "QE-MAY": "Q-MAY",
+            "QE-JUN": "Q-JUN",
+            "QE-JUL": "Q-JUL",
+            "QE-AUG": "Q-AUG",
+            "QE-SEP": "Q-SEP",
+            "QE-OCT": "Q-OCT",
+            "QE-NOV": "Q-NOV",
+            "QE-DEC": "Q-DEC",
             "BQE": "BQ",
-            "BYE": "BY",
-            "h": "H",
+            "BQE-JAN": "BQ-JAN",
+            "BQE-FEB": "BQ-FEB",
+            "BQE-MAR": "BQ-MAR",
+            "BQE-APR": "BQ-APR",
+            "BQE-MAY": "BQ-MAY",
+            "BQE-JUN": "BQ-JUN",
+            "BQE-JUL": "BQ-JUL",
+            "BQE-AUG": "BQ-AUG",
+            "BQE-SEP": "BQ-SEP",
+            "BQE-OCT": "BQ-OCT",
+            "BQE-NOV": "BQ-NOV",
+            "BQE-DEC": "BQ-DEC",
+            "YE": "A",
+            "YS": "AS",
+            "BYE": "BA",
+            "BYS": "BAS",
             "bh": "BH",
             "cbh": "CBH",
+            "h": "H",
             "min": "T",
             "s": "S",
             "ms": "L",
@@ -1816,11 +1935,11 @@ def asbestfreq(data: DataFrame, force_freq: Optional[str] = None) -> DataFrame:
     ndiff = sorted(set(ndiff))
     ngcd = ndiff[0] if len(ndiff) == 1 else reduce(gcd, ndiff)
     if ngcd < 1000:
-        infer_freq = f"{ngcd}{pandas_offset_by_version('n')}"
+        infer_freq = f"{ngcd}{pandas_offset_by_version('ns')}"
     elif ngcd < 1000000:
-        infer_freq = f"{ngcd // 1000}{pandas_offset_by_version('u')}"
+        infer_freq = f"{ngcd // 1000}{pandas_offset_by_version('us')}"
     elif ngcd < 1000000000:
-        infer_freq = f"{ngcd // 1000000}{pandas_offset_by_version('l')}"
+        infer_freq = f"{ngcd // 1000000}{pandas_offset_by_version('ms')}"
     elif ngcd < 60000000000:
         infer_freq = f"{ngcd // 1000000000}{pandas_offset_by_version('s')}"
     elif ngcd < 3600000000000:
